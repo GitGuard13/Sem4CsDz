e37f1f1f4f69e291c03a1fe891a5f45413911ad4
Your Name <nagimov2016@inbox.ru>
2023-03-10 23:32:43 +0300

    Sem4DZ

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 25
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int result = 1;
+// int i = 0;
+
+// while(i < b)
+// {
+//     result *= a;
+//     i++;
+// }
+
+// Console.WriteLine(string.Join(" ", result));
+
+// Задача 27
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// int a = n;
+
+// for (int i = 1; n > 0; i++)
+// {
+//     sum += (n % 10);
+//     n = (n / 10);
+// }
+// Console.WriteLine(string.Join(" ",a,"->",sum));
+
+// Задача 29
+
+// int [] array = new int [8];
+// Random random = new Random();
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(1,10);
+//     Console.WriteLine($"{array[i]}");
+// }
